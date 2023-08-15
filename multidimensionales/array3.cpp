0f9309a537087a185ba0cc0a1df0fd674b3becf2
Cristian David Herrera Medina <cdherreram@unal.edu.co>
2023-08-15 03:06:25 +0000

    Se agrega tarea nueva

--- multidimensionales/array3.cpp
+++ multidimensionales/array3.cpp
@@ -18,10 +18,15 @@ int main() {
     for (int j = 0; j < M; j++) {
       matriz[i][j] = distribucion(generador);
       // Tarea 1: Inicializar la matriz identidad.
-      // Tarea 2: Inicializar la matriz (cuadrada) con valores diferentes de cero sólo en la diagonal, y que estos valores distribuyan Poisson (parámetro 3).
-      // Tarea 3: Inicializar la matriz (cuadrada) como una matriz triangular superior y que los valores distribuyan normal.
-      // Tarea 4: Generar una matriz A (cuadrada) y generar su correspondiente transpuesta B (mínimo 5 x 5).
-      
+      // Tarea 2: Inicializar la matriz (cuadrada) con valores diferentes de
+      // cero sólo en la diagonal, y que estos valores distribuyan Poisson
+      // (parámetro 3). Tarea 3: Inicializar la matriz (cuadrada) como una
+      // matriz triangular superior y que los valores distribuyan normal. Tarea
+      // 4: Generar una matriz A (cuadrada) y generar su correspondiente
+      // transpuesta B (mínimo 5 x 5). Tarea 5: Comprobar con un generador
+      // uniforme entero y con 1000 números entre 1 y 10 que en efecto la
+      // distribución es uniforme (como se hizo en aleatorio.cpp)
+
       std::printf("%5.6f\t\t", matriz[i][j]);
     }
     std::printf("\n");
